2681008387833c08bbeff92414e70b1087c6d055
Ilham Syahid S <ilhamsyahids@gmail.com>
2021-06-06 16:48:12 +0700

    send intent social in ukm

--- app/src/main/java/com/bangkit/waste/ui/UkmActivity.kt
+++ app/src/main/java/com/bangkit/waste/ui/UkmActivity.kt
@@ -1,6 +1,9 @@
 package com.bangkit.waste.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View.GONE
 import androidx.appcompat.app.AppCompatActivity
 import com.bangkit.waste.data.Datasource
@@ -21,15 +24,46 @@ class UkmActivity : AppCompatActivity() {
         }
 
         val dataset = Datasource().loadUkms()
-        
+
         try {
             val ukm = dataset.first { it.productId.contains(productId) }
             binding.nameText.text = ukm.name
             binding.addressText.text = ukm.map.location
+
+            binding.waButton.setOnClickListener {
+                val url = "https://api.whatsapp.com/send?phone=" + ukm.contact.whatsapp + "&text=text=Halo%2C%20saya%20ingin%20bertanya%20suatu%20produk..."
+                val i = Intent(Intent.ACTION_VIEW)
+                i.data = Uri.parse(url)
+                startActivity(i)
+            }
+
+            binding.igButton.setOnClickListener {
+                val url = "https://instagram.com/" + (ukm.contact.instagram?.replace(
+                    '@',
+                    '\u0000'
+                ) ?: '\u0000')
+                val i = Intent(Intent.ACTION_VIEW)
+                i.data = Uri.parse(url)
+                startActivity(i)
+            }
+
+            if (ukm.contact.facebook != null) {
+
+                binding.fbButton.setOnClickListener {
+                    val url = "https://facebook.com/" + ukm.contact.facebook
+                    val i = Intent(Intent.ACTION_VIEW)
+                    i.data = Uri.parse(url)
+                    startActivity(i)
+                }
+            } else {
+                binding.fbButton.visibility = GONE
+            }
+
         } catch (e: Exception) {
             binding.contactButtons.visibility = GONE
+            binding.addressCard.visibility = GONE
             binding.addressText.visibility = GONE
         }
-        
+
     }
 }
